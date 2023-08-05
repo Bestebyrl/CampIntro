5c30cd94b6af5fc31e1de0867a28d8b564e16484
Beste BAYIRLI <Beste BAYIRLI@LAPTOP-AMKKQRMQ>
2023-08-05 15:38:09 +0300

    Proje dosyası ekle.

---
+++ Donguler/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] kurslar = new string[] { "Yazılım Geliştirici",
+                "Programlamaya başlangıç",
+                "Java", "Phyton" };
+
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+            Console.WriteLine("sayfa sonu-footer");
+        }
+    }
+}
